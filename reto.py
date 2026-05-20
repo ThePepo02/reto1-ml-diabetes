509b42e069562b13a779ed1cdc38510a8a188e9d
Angel Luis Pérez Jiménez <aperezj2122@politecnics.barcelona>
2026-05-20 23:32:02 +0200

    pipeline completo - 7 pasos

--- reto.py
+++ reto.py
@@ -1,3 +1,5 @@
+
+# Parte 1: Importar las librrias necesarias para el análisis de datos, visualización y modelado.
 import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
@@ -6,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.metrics import accuracy_score
 
-#Parte 1 filtrar el csv con las columnas que queremos
+#Parte 2 filtrar el csv con las columnas que queremos
 df = pd.read_csv('diabetes.csv')
 columnas = ['Glucose', 'BMI', 'Age', 'Outcome']
 df = df[columnas]
@@ -14,7 +16,7 @@ print("Primeras filas:")
 print(df.head())
 print(f"Tamaño del dataset: {df.shape}")
 
-#Parte 2 calculo de la medio y la desviación normalizando los datos en la misma escala
+#Parte 3 calculo de la medio y la desviación normalizando los datos en la misma escala
 media = np.mean(df['Glucose'])
 desv = np.std(df['Glucose'])
 df['glucosa_norm'] = (df['Glucose'] - media) / desv
@@ -22,7 +24,7 @@ print("\nNormalización")
 print(df[['Glucose', 'glucosa_norm']].head())
 
 
-#Parte 3 Grafico de dispercion Matplotlib
+#Parte 4 Grafico de dispercion Matplotlib
 plt.figure(figsize=(10, 6))
 sc = plt.scatter(df['Glucose'], df['BMI'], c=df['Outcome'], cmap='coolwarm', alpha=0.7)
 plt.colorbar(sc, label='Diabetes (0=Sano, 1=Diabético)')
@@ -34,15 +36,34 @@ plt.tight_layout()
 plt.show()
 
 
-#Parte 4  Entrenamos a la Sklearn para saber que datos tratamos y como estan repartidos
+#Parte 5  Entrenamos a la Sklearn para saber que datos tratamos y como estan repartidos
 x = df[['Glucose', 'Age', 'BMI']]
 y = df['Outcome']
 
-X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 print(f"Total registros : {len(df)}")
 print(f"Entrenamiento : {len(X_train)}")
 print(f"Prueba : {len(X_test)}")
 
 clasificador = DecisionTreeClassifier(max_depth=4, random_state=42)
-clasificador.fit(X_train, Y_train)
+clasificador.fit(X_train, y_train)
+
+
+
+# Parte 6 medir la precisión del modelo
+predicciones = clasificador.predict(X_test)
+precision = accuracy_score(y_test, predicciones)
+print(f"\nPrecisión del modelo: {precision * 100:.2f}%")
+
+
+# Parte 7 - Visualizar el árbol de decisión
+plt.figure(figsize=(20, 10))
+plot_tree(clasificador, feature_names=['Glucosa', 'Edad', 'IMC'], 
+          class_names=['Sano', 'Diabetico'],
+          filled=True,
+          rounded=True,
+          fontsize=9)
+plt.title("Árbol de Decisión - Diagnóstico de Diabetes", fontsize=14)
+plt.tight_layout()
+plt.show()
